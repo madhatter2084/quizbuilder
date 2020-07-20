8e07b0e76d9c70727a5b4febd4520afde882bce4
madhatter <matthew.rikard@gmail.com>
2020-07-19 20:01:13 -0400

    added functions to cycle through questions

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 console.log(choices);
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let question = [
+let questions = [
     {
         question:"Whoo might you find at the Museum?",
         choice1: "Blathers",
@@ -92,4 +92,49 @@ let question = [
         answer: 3
     }
 ]
+
+// Constants
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 10;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    if(availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS){
+        return window.location.assign("/end.html")
+    };
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random()*availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+}
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        console.log(e.target);
+        if(!acceptingAnswers) return;
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+        getNewQuestion();
+    });
+
+});
+
+startGame();
 
